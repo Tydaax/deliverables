d0346003f9b3044e6c301084d8b12b3e45747e22
SrhGyn <s.gayon@cci-formation.nc>
2019-07-18 14:40:57 +1100

    initial commit onpen nc cms

---
+++ sequence5-m4/sgayon/wp-content/themes/lyrical/test.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: Test */ ?>
+
+<?php get_header(); ?>
+
+<div id="presentation"  style=" width:auto ;background-repeat: no-repeat;font-size:40px; color:white ;font-family: sans-serif; ">LE CLUSTER NUMÉRIQUE : UN RÉSEAU D’ENTREPRISES <br> <br><br>DYNAMIQUES <br> <br>
+<h1 style="color:white ;" >ORIGINE DE LA CRÉATION<h1> <br>  
+<p style="color:white ;font-size:26px">● Les travaux de l’ONNC et de l’ACTIC concluants sur la mise en <br> <br>
+place d’une grappe d’entreprises comme outil d’aide à la structuration  <br> <br> de la filière <br> <br>
+● La volonté d’un noyau dur, d’une vingtaine d’entrepreneurs, de <br> <br>
+porter et mettre en oeuvre le projet.</p></div>
+<br><br><br><br>
+<div id="article"  style=" width:auto ; color:white">
+<section>
+<h2 style=" width:auto ;font-size:35px ; color:white">Actualité</h2>
+<h3>Nouméa numérique</h3>
+<img src="https://sortir.nc/wp-content/uploads/noumea-numerique-affiche-juillet2019.png">
+<p>Nouméa numérique aujourd’hui même à la place des cocotiers !</p>
+
+</section>
+
+
+</div>
